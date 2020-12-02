b5646e0f59e8df321fe65d8a6edc671deaeb546e
edwardzhang5 <edwardzhang5@gmail.com>
2020-12-02 12:12:56 -0500

    12/2

--- Song.cpp
+++ Song.cpp
@@ -1,1 +1,28 @@
 #include "Song.h"
+
+Song::Song() {
+	valence = 0;
+	year = 0;
+	acoustic = 0;
+	artists = {};
+	dance = 0;
+	duration = 0;
+	energy = 0;
+	exp = 0;
+	id = "";
+	instrumental = 0;
+	key = 0;
+	liveness = 0;
+	loudness = 0;
+	mode = 0;
+	name = "";
+	popularity = 0;
+	release_year = 0;
+	speech = 0;
+	tempo = 0;
+}
+
+Song::Song(double valence, int year, double acoustic, vector<string> artists, double dance, int duration, double energy, bool exp, string id, double instrumental, int key, double liveness, double loudness, int mode, string name, int popularity, int release_year, double speech, double tempo)
+{
+
+}
--- Song.h
+++ Song.h
@@ -2,12 +2,33 @@
 #include <string>
 #include <vector>
 
+using std::string;
+using std::vector;
+
 class Song
 {
 private:
-
+	double valence;
+	int year;
+	double acoustic;
+	vector<string> artists;
+	double dance;
+	int duration; // in milliseconds
+	double energy;
+	bool exp; // explicit, 0 or 1
+	string id;
+	double instrumental;
+	int key;
+	double liveness;
+	double loudness;
+	int mode;
+	string name;
+	int popularity;
+	int release_year;
+	double speech;
+	double tempo;
 
 public:
-
-
+	Song();
+	Song(double valence, int year, double acoustic, vector<string> artists, double dance, int duration, double energy, bool exp, string id, double instrumental, int key, double liveness, double loudness, int mode, string name, int popularity, int release_year, double speech, double tempo);
 };
