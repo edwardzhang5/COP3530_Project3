83250715e860b984e13e9ec851d33ee317dbca1b
edwardzhang5 <edwardzhang5@gmail.com>
2020-12-02 18:18:28 -0500

    song collection class writing

--- SongCollection.cpp
+++ SongCollection.cpp
@@ -1,4 +1,50 @@
 #include "SongCollection.h"
 
+using namespace std;
 
+SongData::SongData()
+{
+}
+
+void SongData::importSongs()
+{
+	ifstream file;
+	file.open("data.csv");
+	string line;
+
+	getline(file, line); // gets rid of first line of csv
+
+	while (getline(file, line)) {
+		stringstream s (line);
+		string temp;
+
+		getline(s, temp, ',');
+		try {
+			double valence = stod(temp);
+		}
+		catch (std::exception& e) {
+			cout << e.what() << endl;
+		}
+
+		getline(s, temp, ',');
+		try {
+			int year = stoi(temp);
+		}
+		catch (std::exception& e) {
+			cout << e.what() << endl;
+		}
+		
+		getline(s, temp, ',');
+		try {
+			double acoustic = stod(temp);
+		}
+		catch (std::exception& e) {
+			cout << e.what() << endl;
+		}
+
+	
+
+	}
+
+}
 
--- SongCollection.h
+++ SongCollection.h
@@ -1,4 +1,10 @@
 #pragma once
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <exception>
+
 #include "Song.h"
 
 class SongData 
@@ -7,6 +13,6 @@ private:
 	vector<Song> songs;
 
 public:
-
-
+	SongData();
+	void importSongs();
 };
